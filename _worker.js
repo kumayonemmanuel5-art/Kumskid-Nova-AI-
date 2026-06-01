86989f67c6d9f6a3e9d9dc87a45d25089abf97b3
kumayonemmanuel5-art <kumayonemmanuel5@gmail.com>
2026-06-01 22:09:42 +0100

    Add files via upload

---
+++ _worker.js
@@ -0,0 +1,29 @@
+export default {
+  async fetch(request) {
+    if (request.method === 'OPTIONS') {
+      return new Response(null, {
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Methods': 'POST, OPTIONS',
+          'Access-Control-Allow-Headers': 'Content-Type',
+        }
+      });
+    }
+    const body = await request.json();
+    const response = await fetch('https://api.groq.com/openai/v1/chat/completions', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer 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'
+      },
+      body: JSON.stringify(body)
+    });
+    const data = await response.json();
+    return new Response(JSON.stringify(data), {
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*'
+      }
+    });
+  }
+};
